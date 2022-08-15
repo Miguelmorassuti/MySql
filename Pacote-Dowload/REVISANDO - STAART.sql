888bc72cf9dc9da1983f6f4b8c205a00f52af1f4
Miguel Morassuti <miguelguimaraesmorassuti@hotmail.com>
2022-08-15 16:51:46 -0300

    Revisando Database - STAART

---
+++ Pacote-Dowload/REVISANDO - STAART.sql
@@ -0,0 +1,95 @@
+create database STAART;		/*creating database*/
+use STAART;
+
+create table users(
+id_user int auto_increment primary key,
+nome varchar(35) not null,
+username varchar(35) not null unique,
+user_password varchar(35) not null,
+created_at date not null
+)default charset utf8mb4;
+
+create table categories(
+id_category int auto_increment primary key,
+nome varchar(35) not null unique
+);
+
+/*INSERINDO DADOS*/
+insert into categories(nome) values('SERIES');
+insert into categories(nome) values('FILMES');
+insert into categories(nome) values('DESNHOS');
+insert into categories(nome) values('JOGOS AO VIVO');
+insert into categories(nome) values('TERROR');
+insert into categories(nome) values('COMÉDIA');
+insert into categories(nome) values('COMÉDIA ROMANTICA');
+
+/*INSERINDO DADOS*/
+insert into users values
+(default, 'Miguel', 'Miguelzin', '20032010', current_date()), 
+(default, 'Jhulia', 'Jhu', '4321', current_date());
+
+/*ATUALIZANDO DADO DA TABELA*/
+update categories set nome = 'DESENHOS' where id_category =3;
+update categories set nome = 'FUTEBOL' where id_category = 4;
+
+/*UTILIZANDO O DELETE PARA REMOVER ALGUM ELEMENTO DA TABLE*/
+delete from categories where id_category =4;
+
+/*UTILIZANDO ALTER TABLE PARA MUDAR A ESTRUTURA DA TABLE*/
+alter table users add constraint nome unique(nome);
+
+/*OPERADORES*/
+select*from categories where id_category in(3,5); #IN vai buscar os elementos passados no parâmetro
+select*from categories where id_category between 2 and 5; #BETWEEN vai pegar todos elementos do intervalo entre 2 numeros
+select*from categories where id_category != 1; #!= vai buscar tds os elementos diferentes do valor
+select*from categories where id_category = 1; # = vai buscar tds os elementos que possuem o valor passado
+select*from categories where id_category is not null; # IS NOT NULL vai buscar tds os campos onde o valor n é nulo
+select*from categories where nome like 'COM%'; # LIKE vai buscar strings parecidas (n esquecer do %)
+select*from categories where id_category > 1; # > vai buscar tds os valores maior do que o parametro ( < busca o menor)
+
+/*CRIANDO UMA TABLE QUE RECEBE UMA CHAVE ESTRANGEIRA*/
+create table profile(
+id_profile int auto_increment primary key,
+id_user_profile int,
+bio varchar(300) default 'Minha Bio',
+avatar_url varchar(200),
+foreign key(id_user_profile) references users(id_user)
+);
+
+/*INSERINDO DADOS NA MINHA NOVA TABLE*/
+insert into profile values
+(default, 1, default, 'https://google.com.br/avatar');
+
+/*CRIANDO UMA NOVA TABLE*/
+create table conteudo(
+id_conteudo int auto_increment primary key,
+nome varchar(100),
+id_category int,
+duration time,
+foreign key(id_category) references categories(id_category)
+);
+
+/*INSERINDO DADOS NA MINHA NOVA TABLE*/
+insert into conteudo values
+(default, 'PEPPA PIG', 3, '02:00');
+
+/*CRIANDO TABLE QUE LIGA TABELA DE N PARA N*/
+create table users_conteudo(
+id int auto_increment primary key,
+id_users int,
+id_conteudo int,
+time_stopped time,
+note int,
+foreign key(id_users) references users(id_user),
+foreign key(id_conteudo) references conteudo(id_conteudo)
+);
+
+insert into users_conteudo values
+(default, 2, 1, '01:10', 10);
+
+
+
+
+
+
+
